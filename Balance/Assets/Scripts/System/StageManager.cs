96f16f32366e617c92870272ec10f70f7b22ecb3
furu_shun1113 <114386792+ShunFurusawa@users.noreply.github.com>
2024-07-23 12:32:45 +0900

    カメラ統合

--- Balance/Assets/Scripts/System/StageManager.cs
+++ Balance/Assets/Scripts/System/StageManager.cs
@@ -63,6 +63,7 @@ public class StageManager : MonoBehaviour
     [SerializeField] private GameObject SetPos;
     public void SetToStageChild(GameObject obj)
     {
+       
         //親子付けしてもステージの移動に置いて行かれるからサイズとスピード変更
         // Vector3.Scale(obj.transform.localScale, new Vector3(scaleSize, scaleSize, scaleSize));
         Debug.Log("callChange");
@@ -70,6 +71,7 @@ public class StageManager : MonoBehaviour
         obj.GetComponent<PlayerController>().walkSpeed *= 2f;
         obj.GetComponent<PlayerController>().dashSpeed *= 2f;
         obj.transform.position = SetPos.transform.position;
+        return;
         //子オブジェクトのtransform.parentに親にしたいオブジェクトのtransformを代入
         obj.transform.parent = this.transform;
         //new Vector3(0.07f, 4.23f, 0.07f);
