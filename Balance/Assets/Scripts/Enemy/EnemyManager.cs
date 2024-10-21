37feb68d955d7ec24b085f419bc85662f643e69b
Hibiki0130 <g2227075@st.t-kougei.ac.jp>
2024-10-21 20:13:33 +0900

    戦闘シーンつくりました

--- Balance/Assets/Scripts/Enemy/EnemyManager.cs
+++ Balance/Assets/Scripts/Enemy/EnemyManager.cs
@@ -4,23 +4,18 @@ using System.Collections.Specialized;
 //using System.Diagnostics;
 using UnityEngine;
 
-public class EnemyManager: MonoBehaviour
+public class EnemyManager : MonoBehaviour
 {
     [SerializeField] private GameObject stage;
     [SerializeField] private GameObject[] enemys;
     [SerializeField] private GameObject[] trees;
     [SerializeField] private GameObject[] enemySpawnPoints;
 
-    [Header("木の生える範囲")]
-    [SerializeField] private GameObject minimumValue;
-    [SerializeField] private GameObject muximumValue;
-
-    [Header("木の生える数×2")]
-    [SerializeField] private int treeNum = 2;
-
     [Header("敵がスポーンするインターバル")]
     [SerializeField] private float spawnInterval = 3.0f;
 
+    [Header("敵がスポーンする場所の数")]
+    [SerializeField] private int spawnNum = 4;
     //一度にスポーンする数
     private int spawnLimit = 1;
 
@@ -41,8 +36,9 @@ public class EnemyManager: MonoBehaviour
     private Rigidbody enemyRb;
 
     private float enemyCount;
-    private int spawnNum;
-    
+
+    Vector3[] SpawnPos = new Vector3[4];
+
 
     // Start is called before the first frame update
     void Start()
@@ -79,10 +75,6 @@ public class EnemyManager: MonoBehaviour
         ableSpawn = true;
 
         enemyCount = 0;
-
-        GameObject Trees = Instantiate(trees[0]);
-        Vector3 Stage = stage.transform.position;
-        Trees.transform.position = new Vector3(Stage.x + 15, 0, Stage.z + 26);
     }
 
     private void EnemySpawn()
