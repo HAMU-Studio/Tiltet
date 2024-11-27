174456449f76c25956b986c1a64d75f286b84e98
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2024-11-28 03:30:07 +0900

    フェードとシーン遷移システムの作成(途中)

---
+++ Balance/Assets/Scripts/System/FadeSystem/FadeAndSceneTransition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace FadeSystem
+{
+    public class FadeAndSceneTransition : MonoBehaviour
+    {
+        [Header("フェード処理")] public MonoBehaviour fadeHandler;
+        [Header("移動するシーンの名前")] public string nextSceneName;
+
+        private IFadeHandler m_fade;
+        private bool isSceneTransitioning = false;
+
+        private void Start()
+        {
+            //fadeHandlerがIFadeHandlerを実装していればIFadeHandler型に変換して代入
+            m_fade = fadeHandler as IFadeHandler;
+
+            if (m_fade == null)
+            {
+                Debug.LogError("No IFadeHandler attached to fadeHandler");
+            }
+        }
+
+        public void FadeStart()
+        {
+            if (isSceneTransitioning || m_fade == null)
+                return;
+
+            isSceneTransitioning = true;
+            m_fade.StartFadeOut();
+            StartCoroutine(LoadNextSceneAsync());
+
+        }
+
+        private IEnumerator LoadNextSceneAsync()
+        {
+            while (m_fade.IsFadeOutComplete() == false)
+            {
+                yield return null;
+            }
+
+            yield return SceneManager.LoadSceneAsync(nextSceneName);
+        }
+    }
+}
---
+++ Balance/Assets/Scripts/System/FadeSystem/FadeImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FadeSystem
+{
+    public class FadeImage : MonoBehaviour, IFadeHandler
+    {
+        private bool fadeComplete = false;
+
+        public void StartFadeOut()
+        {
+            Debug.Log("フェードアウト開始");
+            fadeComplete = false;
+
+            StartCoroutine(FadeOut());
+        }
+
+        public bool IsFadeOutComplete()
+        {
+            return fadeComplete;
+        }
+
+        private IEnumerator FadeOut()
+        {
+            // 仮
+            yield return new WaitForSeconds(2.0f);
+            fadeComplete = true;
+            Debug.Log("フェードアウト完了");
+        }
+    }
+}
