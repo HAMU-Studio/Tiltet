b895979fb8a29e48b01ba70c9989acce22a8b808
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2024-11-28 01:51:35 +0900

    サウンドテスト用シーンとスクリプト作成

---
+++ Balance/Assets/Scripts/Test/SoundTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Test
+{
+ 
+
+    public class SoundTest : MonoBehaviour
+    {
+        [Header("再生したい音の名前")]
+        [SerializeField] private string[] soundName;
+        
+        private void Update()
+        {
+            for (int i = 0; i < soundName.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+                {
+                    SoundManager.instance.Play(soundName[i]);
+                    Debug.Log(i + "番目のサウンドを再生");
+                }
+            }
+        }
+    }
+}
