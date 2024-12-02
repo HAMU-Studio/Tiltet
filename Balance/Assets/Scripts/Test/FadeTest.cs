8639785d1bb4ed9b59d0e1dd0a2f792b04f5cf01
Hibiki0130 <g2227075@st.t-kougei.ac.jp>
2024-12-02 21:42:09 +0900

    エンカウントしたらファイト

--- Balance/Assets/Scripts/Test/FadeTest.cs
+++ Balance/Assets/Scripts/Test/FadeTest.cs
@@ -11,9 +11,36 @@ namespace Test
        private ThrowawayMethod method = new ThrowawayMethod();
 
        private bool once;
-   //    private ThrowawayMethod method2;
+        private bool getIsEncount;
+
+        EncountManager encountmanager;
+
+        //    private ThrowawayMethod method2;
+        private void Start() 
+        {
+            once = false;
+            getIsEncount = false;
+
+            //EncountManagerにアタッチしてあるEncountManagerのスクリプトから
+            GameObject enemyManager = GameObject.Find("EncountManager");
+            encountmanager = enemyManager.GetComponent<EncountManager>();
+        }
+
         private void Update()
         {
+            //追加しました
+            getIsEncount = encountmanager.isEncount;
+            if (getIsEncount)
+            {
+                if (!once)
+                {
+                    _transition.FadeStart();
+                    GameManager.instance.CurrentState = GameState.EnemyBattle;
+                    once = true;
+                }
+            }
+            //追加しました
+
             if (Input.GetKey(KeyCode.U))
             {
                 if (!once)
