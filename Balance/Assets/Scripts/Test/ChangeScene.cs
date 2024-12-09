854b5206207f1de88cfa7d6b6db4c849c08401d6
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2024-12-10 04:00:24 +0900

    音量調整とSE追加

--- Balance/Assets/Scripts/Test/ChangeScene.cs
+++ Balance/Assets/Scripts/Test/ChangeScene.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,10 +10,12 @@ public class ChangeScene : MonoBehaviour
     public void ToGreenScene()
     {
         GameManager.instance.Restart();
+        SoundManager.instance.Play("CursorDecision");
     }
 
     public void ToSavePoint()
     {
         GameManager.instance.RestartAtSavePoint();
+        SoundManager.instance.Play("CursorDecision");
     }
 }
