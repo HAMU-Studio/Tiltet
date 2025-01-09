bd9569924a2cace3a6268761772356670295130a
Hibiki0130 <g2227075@st.t-kougei.ac.jp>
2025-01-09 17:55:04 +0900

    コインギミックの修正

--- Balance/Assets/Scripts/Gimmick/Coin.cs
+++ Balance/Assets/Scripts/Gimmick/Coin.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public enum FieldType
+    {
+        GREEN,
+        SNOW,
+        VOLCANIC
+    }
+
+    [Header("このコインが置いてある場所")]
+    [SerializeField] private FieldType fieldType;
+
+    CoinManager coinmanager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (fieldType)
+        {
+            case FieldType.GREEN:
+                GameObject greenCoinManager = GameObject.Find("GreenCoinManager");
+                coinmanager = greenCoinManager.GetComponent<CoinManager>();
+                break;
+            case FieldType.SNOW:
+                GameObject snowCoinManager = GameObject.Find("SnowCoinManager");
+                coinmanager = snowCoinManager.GetComponent<CoinManager>();
+                break;
+            case FieldType.VOLCANIC:
+                GameObject volcanicCoinManager = GameObject.Find("VolcanicCoinManager");
+                coinmanager = volcanicCoinManager.GetComponent<CoinManager>();
+                break;
+            default:
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 1.0f, 0);  
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Ground"))
+        {
+            coinmanager.Count();
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Balance/Assets/Scripts/Gimmick/CoinManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using TMPro;
+
+public class CoinManager : MonoBehaviour
+{
+    [Header("コインオブジェクト(取る順番で入れる)")]
+    [SerializeField] private GameObject[] coins;
+    [Header("インスタンスするパーツオブジェクト")]
+    [SerializeField] private GameObject parts;
+
+    //取ったコインの数
+    private int countCoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Set();
+        CoinSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InstanceParts();
+        Debug.Log(countCoin);
+    }
+
+    private void Set()
+    {
+        countCoin = 0;
+
+        for (int i = 0; i < coins.Length; i++)
+        {
+            coins[i].SetActive(false);
+        }
+        parts.SetActive(false);
+    }
+
+    public void Count()
+    {
+        countCoin++;
+        if (countCoin < coins.Length)
+        {
+            CoinSpawn();
+        }
+    }
+
+    private void CoinSpawn()
+    {
+        coins[countCoin].SetActive(true);
+    }
+
+    private void InstanceParts()
+    {
+        if (countCoin >= coins.Length)
+        {
+            parts.SetActive(true);
+        }
+    }
+}
