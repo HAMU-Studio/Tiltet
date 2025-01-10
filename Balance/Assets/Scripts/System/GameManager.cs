f528178c185807b239c6be4ea9e3eb74dc88001e
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-01-10 13:18:32 +0900

    一旦GreenStageで動くように

--- Balance/Assets/Scripts/System/GameManager.cs
+++ Balance/Assets/Scripts/System/GameManager.cs
@@ -142,7 +142,7 @@ public class GameManager : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.G))
         {
-            _sceneManager.FadeStart("MainStage");
+            _sceneManager.FadeStart("GreenStage");
             instance.CurrentState = GameState.Search;
         }
 
