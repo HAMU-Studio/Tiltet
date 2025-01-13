85df77bfb02db0d4a52e3e0e6bf5d831070692c8
Hibiki0130 <g2227075@st.t-kougei.ac.jp>
2025-01-14 05:09:27 +0900

    ポーズ画面仮

--- Balance/Assets/Scripts/System/PauseManager.cs
+++ Balance/Assets/Scripts/System/PauseManager.cs
@@ -5,16 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    //いるかな？
-    /*[Header("タイトルに戻りますか？")]
-    [SerializeField] private GameObject questionAgain;  */
-
+    [Header("ポーズ画面")]
+    [SerializeField] private GameObject pauseImage;
     [Header("タイトルシーンの名前")]
     [SerializeField] private string titleScene;
+    [Header("1P")]
+    [SerializeField] private GameObject firstPlayerImage;
+    [Header("2P")]
+    [SerializeField] private GameObject secondPlayerImage;
+    [SerializeField] private GameObject questionImage;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        pauseImage.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,5 +30,37 @@ public class PauseManager : MonoBehaviour
     public void ToTitleScene()
     {
         SceneManager.LoadScene(titleScene);
+    }
+
+    public void Question()
+    {
+        questionImage.SetActive(true);
+    }
+
+    public void No()
+    {
+        questionImage.SetActive(false);
+    }
+
+    //ポーズ画面表示
+    public void DisplayPauseImage()
+    {
+        pauseImage.SetActive(true);
+    }
+    //ポーズ画面非表示
+    public void HiddenPauseImage()
+    {
+        pauseImage.SetActive(false);
+    }
+
+    //1P画像表示
+    public void Display1P()
+    {
+        firstPlayerImage.SetActive(true);
+    }
+    //2P画像表示
+    public void Display2P()
+    {
+        secondPlayerImage.SetActive(true);
     }
 }
