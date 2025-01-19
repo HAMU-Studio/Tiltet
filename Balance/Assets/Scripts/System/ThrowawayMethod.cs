b189ac43c21593e3b9972f682a9b842a266d66c2
furu_shun1113 <g2227068@st.t-kougei.ac.jp>
2025-01-19 23:38:10 +0900

    プレイヤー微調整

--- Balance/Assets/Scripts/System/ThrowawayMethod.cs
+++ Balance/Assets/Scripts/System/ThrowawayMethod.cs
@@ -2,21 +2,18 @@
 {
     public class ThrowawayMethod
     {
-        // 真偽値の初期値は偽
+        // 初期値は偽
         bool flag;
 
         /// <summary>
         /// 一度だけ処理を行う
         /// </summary>
-        /// <param name="action">処理内容</param>
+        /// <param name="action">処理内用 引数は渡せない</param>
         public void RunOnce(System.Action action)
         {
-            // 真偽値が偽である場合
             if (!flag)
             {
-                // 処理を行う
                 action();
-                // 真偽値を真に切り替える
                 flag = true;
             }
         }
